589a4b5c5269a8c08f8116f5c0d7a01fe08a7d16
F07C <filipe.cabral@aluno.faculdadeimpacta.com.br>
2023-05-26 23:40:10 -0300

    first commit

---
+++ app.py
@@ -0,0 +1,46 @@
+from flask import Flask, jsonify, request
+
+
+
+
+app = Flask(__name__)
+
+carros = [
+    {
+        'id': 1,
+        'Nome': 'Filipe',
+        'Sexo': 'Masculino'
+    },
+    {
+        'id': 2,
+        'Nome': 'Fabiana',
+        'Sexo': 'Feminino '
+    },
+    {
+        'id': 3,
+        'Nome': 'Jose',
+        'Sexo': 'Masculino'
+    },
+    {
+        'id': 4,
+        'Nome': 'Juliana',
+        'sexo': 'Feminino'
+    },
+]
+
+@app.route('/clientes',methods=['GET'])
+def obter_carros():
+    return jsonify(carros)
+
+
+@app.route('/cliente/<int:id>',methods =['GET'])
+def obter_carros_por_id(id):
+    for carro in carros:
+        if carro.get('id') == id:
+            return jsonify(carro)
+
+
+
+app.run(port=5000,host='localhost',debug=True)
+
+
---
+++ connbd.py
@@ -0,0 +1,46 @@
+from flask import Flask, jsonify
+import json
+import requests
+import mysql.connector
+
+app = Flask(__name__)
+
+
+mydb = mysql.connector.connect(host="localhost",user="root",password="password",database="clientes")
+
+#http://127.0.0.1:5000/v1/cliente
+@app.route('/v1/cliente', methods=["GET"])
+def listar():
+    selectAllSql = f"select * from id_clientes"
+    cursor = mydb.cursor()
+    cursor.execute(selectAllSql)
+    resultado = cursor.fetchall()
+    if resultado is None:
+        api_url = "http://localhost:5000/clientes"
+        response = requests.get(api_url)
+        retornaApi = response.json()
+    else:
+        retornaApi = None
+
+    return jsonify(retornaApi)
+
+if __name__ == '__main__':
+    app.run(port=5001)
+    
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
